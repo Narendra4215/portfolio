ef77b8469b3f40480401a6c03d78f8397060b448
Narendra <narendraseerla@gmail.com>
2024-08-16 23:06:50 +0530

    profile-data

---
+++ app/page.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+
+<div className="flex justify-between p-10 text-[26px] md:p-8 md:text-[20px] sm:p-6 sm:text-[18px]">
+  <div>
+    <h1>Narendra Seerla</h1>
+  </div>
+  <div className="flex gap-20 cursor-pointer md:gap-10 sm:gap-5">
+    <div>work</div>
+    <div>touch me</div>
+  </div>
+</div>
+
+      <div>
+
+        <div className="profile text-center text-[50px] m-[45px]">
+          profile
+        </div>
+        <div className="w-1/2 p-[20px] tracking-[2px] [word-spacing:12px] ml-[10%]">
+          <p>Highly motivated and professional Front-End Developer , AEM
+            Developer and ReactJs Developer with 1.5+ years of experience.
+            Strong technical skills in complex web application development.
+            Good experiencing in developing single-page applications using
+            Reactjs and redux. Strong foundation in Javascript along with ES6.
+            Experience developing highly interactive web applications utilizing JS,
+            HTML5, SCSS, JSON, Bootstrap and integrating Restful API’s and
+            GraphQL. Good relationship management, technical and
+            communication abilities with an inherent ability to actively
+            participate in a team and make a difference. Worked in in various
+            control systems - GIT.</p>
+        </div>
+      </div>
+
+    </div>
+
+  );
+}
